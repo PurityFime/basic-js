30481c1495d32306eeea5eef36dea72e364915cf
PurityFime <71134621+PurityFime@users.noreply.github.com>
2020-11-01 17:11:01 +0300

    init: upload code

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!Array.isArray(arr)) {
+    throw new Error;
+  }
+  else if (arr.length === 0) {
+    return [];
+  }
+  else {
+    let control = arr.find(str => typeof str === 'string');
+    let initArray = arr.filter(num => typeof num !== '--double-next' && num !== '--discard-next' && num !== '--double-prev' && num !== '--discard-prev');
+    let controlIdx = arr.indexOf(control);
+    let resultArray =[];
+    if (control === '--double-next') {
+      initArray.forEach(function(num) {
+        if (num !== initArray[controlIdx]) {
+          resultArray.push(num)
+        }
+        else {
+          resultArray.push(num);
+          resultArray[controlIdx + 1] = num;
+        }
+        return resultArray;
+      })
+    } 
+    else if (control === '--discard-next') {
+      initArray.forEach(function(num) {
+        if (num !== initArray[controlIdx]) {
+          resultArray.push(num)
+        }
+        return resultArray;
+  
+      })
+    }
+    else if (control === '--double-prev') {
+      initArray.forEach(function(num) {
+        if (num !== initArray[controlIdx-1]) {
+          resultArray.push(num);
+        }
+        else {
+          resultArray.push(num);
+          resultArray.push(num);
+        }
+        return resultArray;
+      })
+    }
+    else if (control === '--discard-prev') {
+      initArray.forEach(function(num) {
+        if (num !== initArray[controlIdx-1]) {
+          resultArray.push(num);
+        }
+        return resultArray;
+      })
+    }
+  }
+
 };
