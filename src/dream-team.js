6a8709a4d1c13d8665752dd746e817622fdd5009
PurityFime <71134621+PurityFime@users.noreply.github.com>
2020-11-01 17:10:00 +0300

    init: upload code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (Array.isArray(members)) {
+    const strings = members.filter(member => typeof member === 'string');
+    let arr = [];
+    let newArray = [];
+    let teamName;
+    for (let name of strings) {
+      arr = name.split('');
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === ' ') {
+          arr.splice(i, 1)
+          i--;
+        }  
+      }
+
+      newArray.push(arr.join('').toUpperCase().split('')[0]);
+    }
+    return newArray.sort().join('').toUpperCase();
+} else {
+    return false;
+  }
+
 };
