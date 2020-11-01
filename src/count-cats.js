58ead909ddf1dd90463c930c1106e30dab5b18c0
PurityFime <71134621+PurityFime@users.noreply.github.com>
2020-11-01 17:26:31 +0300

    fix: update code

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-return backyard.flat().filter(cat => cat === '^^').length;
+module.exports = function countCats(backyard) {
+  return backyard.flat().filter(cat => cat === '^^').length;
 };
